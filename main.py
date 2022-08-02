23f490ded5dc4f36b50975f295b104a77cce3d67
Melissa <melissanogueira15@gmail.com>
2022-08-02 14:59:01 -0300

    first

---
+++ main.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+#criar a 1° página do site
+#route -> melanoma.com.br/
+#função -> o que será exibido na página
+
+@app.route("/")  #atribuir uma nova funcionalidade para a função abaixo
+def DiagnosticoMelanoma():
+    return render_template("DiagnosticoMelanoma.html")
+
+
+
+
+#colocar o site no ar
+if __name__ == "__main__":   #executar o código quando rodar diretamente
+    app.run(debug=True)
+
+    # servidor do heroku
